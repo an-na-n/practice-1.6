857136fd069d69d000ed241516c89fcc43b29695
Anna Nikolaeva <nikolaevaa.94@mail.ru>
2025-04-02 02:29:26 +0300

    SCSS continuing

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
+import Swiper from 'swiper'
+import { Pagination } from 'swiper/modules'
+Swiper.use([Pagination])
+
 import '../scss/style.scss'
 
-console.log('Works!')
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  direction: 'horizontal',
+  loop: false,
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets'
+  }
+})
